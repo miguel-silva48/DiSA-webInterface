e7d673f03f66d73819c2f206083a80cd991253d6
Miguel da Silva Pinto <miguel.silva48@ua.pt>
2024-03-17 10:36:53 +0000

    Feat(webInterface): Created home layout similar to figma and changes to icons

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,40 +1,47 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { baseUrl } from "../../main.jsx";
+import React from "react";
+import Navbar from "../layout/Navbar.jsx";
+
+import side_image from "/side_image.png";
 
 const HomePage = () => {
-
   return (
-    <div>
-      <div className="flex w-screen flex-col items-start gap-2.5">
-        <div className="w-full h-[63rem] bg-white">
-          <div className="w-[35rem] h-[31.5rem] absolute left-[9.5rem] top-[14.4rem]">
-            <p className="font-sans text-6xl font-extrabold bg-gradient-to-b from-[#6941C6] to-[#27164F] bg-clip-text text-transparent">
-              Welcome to DiSA
-            </p>
-            <p className="text-2xl text-gray-600 mt-2 mb-4">
-              Digitally Signed Archive is a platform that allows you to save and sign files with authenticity and integrity guarantee.
-            </p>
-            <form className="w-[34.5rem] shrink-0">
-              <div className="flex flex-col justify-center items-start shrink-0">
-                <div className="flex p-5 pr-6 items-center flex-[1_0_0] self-stretch rounded-2xl border border-gray-400 bg-gray-50 mt-4">
-                  <input
-                    placeholder="Insert link here"
-                  ></input>
-                  <button
-                    type="button"
-                    className="flex w-[13rem] h-[3.35rem] p-4 flex-col gap-2.5 shrink-0 rounded-[0.625rem] bg-gray-950"
-                  >
-                    <div className="flex justify-center items-center gap-2">
-                      <p className="text-white text-base font-bold text-white">
-                        Confirm
-                      </p>
-                    </div>
-                  </button>
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      <div className="flex flex-row w-full h-full">
+        <div className="w-[60%] h-full p-10">
+          <h1 className="font-sans text-6xl font-extrabold bg-gradient-to-b from-[#6941C6] to-[#27164F] bg-clip-text text-transparent">
+            Welcome to DiSA
+          </h1>
+          <h2 className="text-3xl text-gray-600 mt-2 mb-4">
+            Digitally Signed Archive is a platform that allows you to save and sign files with authenticity and integrity guarantee.
+          </h2>
+          <div className="flex justify-center">
+          <form className="w-2/3 border border-black rounded-lg p-4">
+            <h2 className="text-2xl font-bold text-gray-600 text-center mb-4">Want to submit files? <u className="text-purple-500">Find out how</u></h2>
+            <hr className="border-t border-purple-500 mb-4" />
+            <div className="flex flex-col justify-center items-start mb-4">
+              <p className="text-xl text-gray-600">Please insert your link below:</p>
+            </div>
+            <div className="flex items-center">
+              <input 
+                className="w-4/5 border border-gray-400 rounded p-2 mr-4 flex-grow" 
+                placeholder="Insert link here"></input>
+              <button
+                type="button"
+                className="flex p-4 flex-col gap-2.5 shrink-0 rounded-[0.625rem] bg-gray-950"
+              >
+                <div className="flex justify-center items-center gap-2">
+                  <p className="text-white text-base font-bold">
+                    Confirm
+                  </p>
                 </div>
-              </div>
-            </form>
+              </button>
+            </div>
+          </form>
           </div>
+        </div>
+        <div className="w-[40%] h-full p-10">
+          <img src={side_image} className="justify-center items-center" alt="Side Image" />
         </div>
       </div>
     </div>
