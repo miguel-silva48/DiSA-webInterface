a4e5261c862dcdb941cdbac6b8044b3fa4e0f935
Miguel da Silva Pinto <miguel.silva48@ua.pt>
2024-04-21 20:29:46 +0100

    Fix(webInterface/dashboard): Allowed user to copy token in order to login the CLI

--- src/components/pages/DashboardPage.jsx
+++ src/components/pages/DashboardPage.jsx
@@ -1,19 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../layout/Navbar.jsx";
 import Footer from "../layout/Footer.jsx";
 import Background from "../layout/Background.jsx";
 import DocumentSetCard from "../layout/DocumentSetCard.jsx";
 
 const DashboardPage = () => {
+  const user_token = sessionStorage.getItem("access_token") || "";
 
-// Redirect to login page if user is not logged in and tries to access this
-useEffect(() => {
-  if (sessionStorage.getItem("access_token") === null) {
-    window.location.href = "/login";
+  // Redirect to login page if user is not logged in and tries to access this
+  useEffect(() => {
+    if (sessionStorage.getItem("access_token") === null) {
+      alert("You must be logged in to access this!");
+      window.location.href = "/login";
+    }
   }
-}
-, []);
+    , []);
 
+  //TODO - fetch user's documents and display them
+
+  // Make token available to copy so that user can login the CLI
+  const handleLinkCopy = () => {
+    navigator.clipboard.writeText(user_token).then(() => {
+      setCopySuccess(true);
+      alert("User token copied to clipboard!");
+    }).catch((error) => {
+      alert("Error copying user token to clipboard!");
+      console.error('ERROR copying link:', error);
+    });
+  };
 
   return (
     <div>
@@ -27,6 +41,15 @@ useEffect(() => {
           <h2 className="text-3xl text-gray-600 mt-2 mb-4">
             Here you can check your submitted files, manage their accesses and much more!
           </h2>
+          <p className="text-2xl text-gray-600">
+            To use the CLI tool,           
+            <span 
+              className="text-purple-500 underline cursor-pointer ml-2 mr-2"
+              onClick={handleLinkCopy}
+            >copy your token</span> 
+            and paste it in the CLI tool.
+          </p>
+
           <DocumentSetCard />
           <DocumentSetCard />
           <DocumentSetCard />
