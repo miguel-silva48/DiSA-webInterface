df69e44bd1df39985059862ad2c90d10fa98dca2
Miguel da Silva Pinto <miguel.silva48@ua.pt>
2024-05-01 21:05:36 +0100

    Feat(webInterface): added layout and frontend logic for managing emails shared with

--- src/components/layout/DocumentSetCard.jsx
+++ src/components/layout/DocumentSetCard.jsx
@@ -1,40 +1,35 @@
 import React, { useState } from 'react';
 import { API_BASE_URL } from "../../constants/index.jsx";
 import { useNavigate } from 'react-router-dom';
+import SharingList from '../layout/SharingList.jsx';
 
-import { RiSettingsFill } from 'react-icons/ri';
+import { RiSettingsFill, RiShareFill } from 'react-icons/ri';
 
 const DocumentSetCard = ({collection}) => {
-  const [copySuccess, setCopySuccess] = useState(false);
-
   const navigate = useNavigate();
+  const [showSharingModal, setShowSharingModal] = useState(false);
 
   const cardName = collection.name? collection.name : 'Untitled';
   const sharingState = collection.share_state? collection.share_state : 'Unknown';
   const date = collection.date? collection.date : 'Unknown';
-  const link = 'https://doi.org/10.5281/';
+  const link = collection.id? 'https://localhost:3000/shared?col_uuid=' + collection.id : null;
 
   const handleManageCollection = () => {
     navigate('/dashboard/collection', { state: { collection: collection } });
   };
 
-  const handleLinkCopy = () => {
-    navigator.clipboard.writeText(link).then(() => {
-      setCopySuccess(true);
-      setTimeout(() => setCopySuccess(false), 3000);
-    }).catch((error) => {
-      console.error('ERROR copying link:', error);
-    });
+  const handleShareCollection = () => {
+    setShowSharingModal(true);
   };
 
   return (
-    <div className="flex justify-center p-4">
+    <div className="flex justify-center text-gray-600 p-4">
       <div className="w-2/3 border-2 border-gray-600 rounded-lg bg-white p-4">
-        <div className="flex items-center justify-between mb-4 text-2xl font-bold text-gray-600">
+        <div className="flex items-center justify-between mb-4 text-2xl font-bold">
           <h2>{cardName}</h2>
           <button onClick={handleManageCollection}>
             <div className="flex gap-2 border-2 border-black rounded-lg ">
-              <p className="text-xl text-gray-600">Manage collection</p>
+              <p className="text-xl font-bold">Manage collection</p>
               <RiSettingsFill /></div>
           </button>
         </div>
@@ -42,8 +37,8 @@ const DocumentSetCard = ({collection}) => {
         <hr className="border-t border-purple-500 mb-4" />
 
         <div className="flex gap-20 justify-between">
-          <p className="text-xl text-gray-600">Submission Date: {date}</p>
-          <p className="text-xl text-gray-600">
+          <p className="text-xl">Submission Date: {date}</p>
+          <p className="text-xl">
             Share State:
             <span className={
               sharingState === 'public' ? 'text-green-600' :
@@ -54,17 +49,13 @@ const DocumentSetCard = ({collection}) => {
             }> {sharingState}
             </span>
           </p>
-          <div className="flex gap-2">
-            <p className="text-xl text-gray-600">Link: </p>
-            <span
-              className="text-xl text-purple-500 underline cursor-pointer"
-              onClick={handleLinkCopy}
-            >
-              {link}
-            </span>
-          </div>
+          <button onClick={handleShareCollection}>
+            <div className="flex gap-2 border-2 border-black rounded-lg ">
+              <p className="text-xl font-bold">Share collection</p>
+              <RiShareFill className='text-2xl'/></div>
+          </button>
         </div>
-        {copySuccess && <p className="flex justify-end text-xl text-green-600">Link copied!</p>}
+        {showSharingModal && <SharingList collection={collection} onClose={() => setShowSharingModal(false)} />}
       </div>
     </div>
   );
---
+++ src/components/layout/SharingList.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import { RiClipboardFill, RiDeleteBinFill, RiAddCircleFill } from 'react-icons/ri';
+
+const SharingList = ({ collection, onClose }) => {
+  const link = `https://localhost:3000/shared?col_uuid=${collection.id}`;
+  const [sharedEmails, setSharedEmails] = React.useState(["example@example.pt", "example2@example.pt"]);
+  const [newEmail, setNewEmail] = React.useState("");
+  const [copySuccess, setCopySuccess] = React.useState(false);
+
+  const addEmail = () => {
+    // If email is empty, do nothing
+    if (newEmail.trim() === "") return;
+
+    // If it is not an email, alert with error
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(newEmail.trim())) {
+      alert("Invalid email, please enter a valid email and try again.");
+      setNewEmail("");
+      return;
+    }
+
+    // If email is in the list, alert with error
+    if (sharedEmails.includes(newEmail.trim())) {
+      alert("Email already in the list.");
+      setNewEmail("");
+      return;
+    }
+
+    // Add email to the list
+    //TODO make api call adding this
+    setSharedEmails(prevEmails => [...prevEmails, newEmail.trim()]);
+    setNewEmail("");
+  };
+
+  const removeEmail = (index) => {
+    //TODO make api call removing this
+    setSharedEmails(prevEmails => prevEmails.filter((email, i) => i !== index));
+  };
+
+  const handleLinkCopy = () => {
+    navigator.clipboard.writeText(link).then(() => {
+      setCopySuccess(true);
+      setTimeout(() => setCopySuccess(false), 3000);
+    }).catch((error) => {
+      console.error('ERROR copying link:', error);
+    });
+  };
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
+      <div className="bg-white p-6 rounded-lg relative min-w-40 min-h-screen-3/4">
+        <button className="absolute top-0 right-0 mt-2 mr-2 text-gray-600" onClick={onClose}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+        <h2 className="text-2xl font-bold mb-8">Collection: {collection.name}</h2>
+        <div className="flex justify-between items-center mb-8 gap-40">
+          <h3 className="text-xl">Shared with:</h3>
+          <button
+            className="flex items-center text-white font-bold rounded-lg bg-purple-600 hover:bg-indigo-500 px-3 py-3"
+            onClick={handleLinkCopy}>
+            <RiClipboardFill className="text-2xl mr-2" />
+            Copy Share Link
+          </button>
+          
+        </div>
+        {copySuccess && <span className="text-green-600">Link copied to clipboard!</span>}
+        <hr className="border-t border-purple-600 mt-2 mb-2" />
+        {sharedEmails.length > 0 ? (
+          <ul>
+            {sharedEmails.map((email, index) => (
+              <li key={index} className="flex items-center justify-between py-2 ml-4">
+                <span>{email}</span>
+                <button onClick={() => removeEmail(index)} className="flex items-center rounded-lg text-white bg-red-600 hover:bg-red-400 px-3 py-1">
+                  <RiDeleteBinFill className="text-xl mr-2" />
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>You haven't shared this collection with anyone yet!</p>
+        )}
+        <hr className="border-t border-purple-600 mt-2 mb-8" />
+        <div className="flex mt-4 justify-center">
+          <input
+            type="email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+            placeholder="Enter new email"
+            className="border border-gray-400 p-2 mr-2 rounded-lg"
+          />
+          <button onClick={addEmail} className="flex items-center bg-blue-500 hover:bg-indigo-500 text-white px-4 py-2 rounded-lg">
+            <RiAddCircleFill className="text-xl mr-2" />
+            Add Email
+          </button>
+        </div>
+        <button className="bg-gray-500 text-white px-4 py-2 rounded-lg mt-4 ml-2" onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default SharingList;
