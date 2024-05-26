2165c21f815dd8b04fb3dda4024f27881898588b
Tiago Figueiredo <tiagofigueiredo2003@gmail.com>
2024-05-26 22:11:00 +0100

    feat: login, and register with cmd
        This commit is a hand made squash of:
            5a2e77d feat: catch 401
            8f3f748 fix: login token being set in the wrong place
            72215e5 fix: save to sessionStorage instead of localStorage, add max-h to login and register, to fix taller screens
            75a9e92 feat: show error alert when the session does not match
            87b5eb8 feat: Login with cmd
            387600f feat: Register with cmd
    
        made due to the leak of a secret

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,25 @@ import CollectionPage from "./components/pages/CollectionPage";
 import AboutPage from "./components/pages/About";
 
 import ErrorPage from "./components/pages/ErrorPage";
+import CMDRegister from "./components/pages/Cmd";
+import CMDLogin from "./components/pages/CMDLogin";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/dashboard/collection" element={<CollectionPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/cmdLogin" element={<CMDLogin />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/dashboard/collection" element={<CollectionPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/cmd" element={<CMDRegister />} />
+                <Route path="*" element={<ErrorPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,106 +1,50 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { API_BASE_URL } from "../../constants/index.jsx";
+import { Link } from "react-router-dom";
 
 import Navbar from "../layout/Navbar.jsx";
 import Footer from "../layout/Footer.jsx";
 import Background from "../layout/Background.jsx";
 
+import Auth from "/autenticacao_gov.png";
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    username: "",
-    password: ""
-  });
-  const [formErrors, setFormErrors] = useState({});
+    let client_id = "";
+    let state;
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleLogin = async () => {
-    const newErrors = {};
-    if (!formData.username) {
-      newErrors.username = "Username field is required!";
-    }
-    if (!formData.password) {
-      newErrors.password = "Password field is required!";
-    }
-
-    // No errors, send the POST request
-    if (Object.keys(newErrors).length === 0) {
-      try {
-        const params = new URLSearchParams();
-        params.append('username', formData.username);
-        params.append('password', formData.password);
-
-        const response = await fetch(API_BASE_URL + "/users/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: params.toString(),
-        });
-
-        if (!response.ok) {
-          alert("Invalid credentials!");
-          return;
-        }
-
-        const data = await response.json();
-        console.log("LOGIN RESPONSE:", data);
-        if (response.ok) {
-          sessionStorage.setItem('username', formData.username);
-          sessionStorage.setItem('access_token', data.access_token);
-          alert("User logged in successfully!");
-          navigate("/dashboard");
-        }
-      } catch (error) {
-        console.error('Error logging in:', error);
-      }
+    if (!document.cookie.includes("state")) {
+        state = crypto.randomUUID();
+        document.cookie = `state=${state}; SameSite=Strict; Secure`;
     } else {
-      setFormErrors(newErrors);
+        state = document.cookie.split("=")[1]
     }
-  };
 
-
-  return (
-    <div className="flex flex-col h-screen">
-      <Background />
-      <div className="flex flex-col flex-grow">
-        <Navbar />
-        <div className="flex justify-center h-full p-20">
-          <div className="w-[60%]">
-            <h1 className="font-sans text-6xl font-extrabold bg-gradient-to-b from-[#6941C6] to-[#27164F] bg-clip-text text-transparent text-center mb-8 p-2">
-              Log in to your DiSA account
-            </h1>
-            <form className="w-full max-w-lg mx-auto border  bg-white rounded-lg p-8 shadow-md">
-              <div className="mb-6">
-                <label htmlFor="username" className="block text-gray-600 font-bold mb-2">Username</label>
-                <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} className="w-full border border-gray-300 rounded px-3 py-2 leading-tight focus:outline-none focus:shadow-outline" placeholder="Insert your username here" />
-                {formErrors.username && <p className="text-red-500">{formErrors.username}</p>}
-              </div>
-              <div className="mb-6">
-                <label htmlFor="password" className="block text-gray-600 font-bold mb-2">Password</label>
-                <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} className="w-full border border-gray-300 rounded px-3 py-2 leading-tight focus:outline-none focus:shadow-outline" placeholder="Insert your password here" />
-                {formErrors.password && <p className="text-red-500">{formErrors.password}</p>}
-              </div>
-              <div className="flex items-center justify-between">
-                <Link to="/" className="text-purple-600 hover:text-indigo-400">Back to HomePage</Link>
-                <button type="button" onClick={handleLogin} className="bg-purple-600 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
-              </div>
-            </form>
-          </div>
+    return (
+        <div className="flex flex-col h-screen">
+            <Background />
+            <div className="flex flex-col flex-grow">
+                <Navbar />
+                <div className="flex justify-center items-center h-full p-10">
+                    <div className="w-[500px] bg-white rounded-xl shadow-md flex flex-col items-center justify-center p-5 opacity-80 max-h-[500px]">
+                        <h1 className="font-sans text-6xl font-extrabold bg-gradient-to-b from-[#6941C6] to-[#27164F] bg-clip-text text-transparent text-center mb-8 mt-20">
+                            Log in to your DiSA account
+                        </h1>
+                        <div className="flex-grow">
+                        </div>
+                        <div className="mb-20 p-5 flex flex-col items-center">
+                            <p className="font-semibold text-center mb-4">
+                                This app uses Autenticacao.gov to authenticate accounts, please click the button bellow to login.
+                            </p>
+                            <Link
+                                to={`https://preprod.autenticacao.gov.pt/oauth/askauthorization?redirect_uri=http://127.0.0.1:3000/cmdLogin&client_id=${client_id}&state=${state}&scope=http://interop.gov.pt/MDC/Cidadao/NomeProprio%20http://interop.gov.pt/MDC/Cidadao/NIC&response_type=token`}
+                                className="bg-purple-200 rounded-xl p-5 shadow-md hover:bg-purple-400 hover:shadow-xl">
+                                <img src={Auth} className="h-16" alt="Autenticação com Chave Móvel Digital" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default LoginPage;
